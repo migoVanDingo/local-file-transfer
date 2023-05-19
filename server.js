b388f74c67f494d9f1c83448db9ff37a00f9189d
migo <migo.vandingo@gmail.com>
2023-05-18 21:21:24 -0600

    update server.js

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const app = express()
 
 global.__basedir = __dirname
 
-
-
-app.use(cors({ origin: "http://localhost:3000"}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+)
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
 
 const initRoutes = require("./routes/index")
 
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 initRoutes(app)
 
 const PORT = 3002
 app.listen(PORT, () => {
-    console.log("Server listening on port: " + PORT)
+  console.log("Server listening on port: " + PORT)
 })
